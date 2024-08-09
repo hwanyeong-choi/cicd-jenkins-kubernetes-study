9393878a8b67303c0b3b1e55410e78919f4dd87d
DESKTOP-K4E22JK\THINKPAD-WINDOWS <hoany1215@gmail.com>
2024-08-09 12:31:44 +0900

    jenkins-kubernetes Test Spring boot Web Api Server

---
+++ src/main/java/com/includehoany/hellojenkinskubernetes/HellojenkinsKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.includehoany.hellojenkinskubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellojenkinsKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellojenkinsKubernetesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/includehoany/hellojenkinskubernetes/HellojenkinsKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.includehoany.hellojenkinskubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellojenkinsKubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
